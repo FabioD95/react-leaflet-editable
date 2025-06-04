d5381917596ef94ac338325f5ea4a6a771398b28
FabioD95 <fabio.delrio95@gmail.com>
2025-06-04 16:40:18 +0200

    main import

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import LayersControl from "./LayersControl.tsx";
+// import LayersControl from "./LayersControl.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <LayersControl />
+    <App />
+    {/* <LayersControl /> */}
   </StrictMode>
 );
